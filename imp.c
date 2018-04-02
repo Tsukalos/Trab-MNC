7d9ccec33e099d291673b8f8808ee7ce6503e63e
Pedro Lamkowski <pedrolamkowski@gmail.com>
2018-04-01 21:35:18 -0300

    Re-wrote imp.c

--- imp.c
+++ imp.c
@@ -7,16 +7,4 @@
 
 
 int main(){
-    double x;
-    te_variable vars[] = {{"x",&x}};
-    int err;
-    te_expr *ex = te_compile("x^2 + x",vars,1,&err);
-    int i = 0;
-    for(i = 0; i < 5; i++){
-        x = i;
-        printf("%.4f\n",te_eval(ex));
-    }
-    getch();
-    te_free(ex);
-    return 0;
 }
