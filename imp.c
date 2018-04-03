2b91b65f810bc9dd770806b52ef5da5d9db46187
Pedro Lamkowski <pedrolamkowski@gmail.com>
2018-04-02 22:46:04 -0300

    Inicio da função de bisseção

--- imp.c
+++ imp.c
@@ -3,8 +3,14 @@
 #include "tinyexpr.h"
 
 
-
-
+int bissecao(double e, int iMax, te_expr *f, double a, double b, int *k, double *r, te_variable vars[]){
+    int *x = vars[0].address;
+    *x = a;
+    double fa = te_eval(*f);
+    *x = b;
+    double fb = te_eval(*f);
+}
 
 int main(){
+
 }
