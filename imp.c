1c74714e1f78d76a517347fc6093767499f93240
Pedro Lamkowski <pedrolamkowski@gmail.com>
2018-04-07 18:21:49 -0300

    Adicionados posição falsa e posição falsa modificado

--- imp.c
+++ imp.c
@@ -364,6 +364,113 @@ int bissecao(double e, int iMax, te_expr *f, double a, double b, int *k, double 
     
 }
 
+int posicaofalsa(double e, int iMax, te_expr *f, double a, double b, int *k, double *r){
+    printf("\nPosicao falsa");
+	double x;
+    double fx;
+    int flag = 0;
+    if(!bolzano(f,a,b)){
+        printf("\nO intervalo não converge...\n");
+        return 1;
+    }
+    while(!flag && *k <= iMax){
+        *k += 1;
+        printf("\nInteracao %d",*k);
+        printf("\nk = %d",*k);
+        x = (a*calcF1(f,b) - b*calcF1(f,a))/(calcF1(f,b) - calcF1(f,a)); //criando novo x;
+        printf("\nx = %f",x);
+        fx = calcF1(f,x);        
+        printf("\n |f(x)| = |%f| ",fx);
+        if(fabs(fx) < e) flag = 1; //fabs = modulo checar f(x)
+	
+        if(bolzano(f,a,x)){
+            b = x; // se fa*fx < 0
+        }else{
+            a = x; // se fa*fx > 0
+        }
+        printf(" |b-a| = |%f|",fabs(b-a));
+        if(fabs(b - a) < e) flag = 1; //|b-a| checar intervalo   
+	}
+    *r = x;
+
+    if(*k > iMax){
+        printf("\nO número máximo de interações foi alcançado.\n");
+        return 2;
+    }
+    
+    return 0;
+    
+}
+
+
+int posicaofalsamodificada(double e, int iMax, te_expr *f, double a, double b, int *k, double *r){
+    printf("\nPosicao falsa modificada");
+	double x=0;
+    double fx;
+    double xp;
+    int flag = 0;
+    int flag2=0;
+    int fixo=0; //flag de fixo
+    if(!bolzano(f,a,b)){
+        printf("\nO intervalo não converge...\n");
+        return 1;
+    }
+    while(!flag && *k <= iMax){
+        *k += 1;
+        printf("\nInteracao %d",*k);
+        printf("\nk = %d",*k);
+        if(x) xp = x;
+        else xp=a;
+        if(flag2)
+        {
+        	if(!fixo)//fixo = a
+        	{
+        		printf("\t f(a) = f(a)/2");
+        		x = (a*calcF1(f,b) - b*calcF1(f,a)/2)/(calcF1(f,b) - calcF1(f,a)/2); 
+
+			}
+        	else{
+        		printf("\t f(b) = f(b)/2");
+        		x = (a*calcF1(f,b)/2 - b*calcF1(f,a))/(calcF1(f,b)/2 - calcF1(f,a)); 
+			}
+		}
+		else x = (a*calcF1(f,b) - b*calcF1(f,a))/(calcF1(f,b) - calcF1(f,a)); 
+        //criando novo x;
+        flag2=0;
+        printf("\nx = %f",x);
+        fx = calcF1(f,x);        
+        printf("\n |f(x)| = |%f| ",fx);
+        if(fabs(fx) < e) flag = 1; //fabs = modulo checar f(x)
+	
+        if(bolzano(f,a,x)){
+            b = x; // se fa*fx < 0
+            fixo = 0; // fixo = a
+        }else{
+            a = x; // se fa*fx > 0
+            fixo = 1;//fixo = b
+        }
+        printf(" |b-a| = |%f|",fabs(b-a));
+        if(fabs(b - a) < e) flag = 1; //|b-a| checar intervalo
+		printf("\tXp = %f",xp);  
+        if(calcF1(f,xp) * calcF1(f,x) > 0 )
+        {
+        	flag2 = 1;
+		}
+		printf("\t[a b] = [%f   %f]",a,b);
+		 
+	}
+    *r = x;
+
+    if(*k > iMax){
+        printf("\nO número máximo de interações foi alcançado.\n");
+        return 2;
+    }
+    
+    return 0;
+    
+}
+
+
 void pre_bissecao(){
     char ex[50];
     te_expr *f = verifica_gera_expr(ex);
